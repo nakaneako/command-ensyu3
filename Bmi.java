1653359d5cc94eb95f74efe8241308613c88e8e4
Ako Nakane <146405232+nakaneako@users.noreply.github.com>
2023-11-24 11:31:16 +0900

    Bmi.javaをコンパイル

--- Bmi.java
+++ Bmi.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+public class Bmi {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("1人目の身長を入力してください: ");
